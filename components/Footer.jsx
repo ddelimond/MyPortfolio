32a64dfbc1fc40f958af09ea311cb806147a5ba9
Darren Delimond <darrendelimond@gmail.com>
2023-05-13 05:57:37 -0400

    created footer component also commiting final index.js

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import moment from 'moment'
+
+const Footer = () => {
+    let year = moment().format('YYYY')
+    return (
+        <footer class="absolute w-full h-20 bottom-[-10%] flex flex-wrap text-center justify-center items-center bg-white "> &copy; {year} All rights reseved Darren Delimond</footer>
+    )
+}
+
+export default Footer
