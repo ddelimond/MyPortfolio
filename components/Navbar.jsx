91ae1c1bb04295818f1b6c35f9ceb45f804ef04f
Darren Delimond <darrendelimond@gmail.com>
2023-08-26 18:53:57 -0400

    =updated skills and resume to better reflect me

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -47,7 +47,7 @@ return (
 
                     <Toggle />
 
-                    <Link href='/Darren Delimond Resume 2023.pdf' download='Darren Delimond Resume'>{<FaFileDownload/>}</Link>
+                    <a href='/Darren Delimond Resume 2023.pdf' download='Darren Delimond Resume'>{<FaFileDownload/>}</a>
 
                     <section className='mobileMenu cursor-pointer md:hidden'>
                         <AiOutlineMenu size={25} onClick={handleOpen}/>
