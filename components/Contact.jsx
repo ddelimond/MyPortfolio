167cbc39b1498aa80c781cfaff33c4c327231562
Darren Delimond <darrendelimond@gmail.com>
2023-12-05 07:01:09 -0500

    =updated chevron color

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -97,7 +97,7 @@ const Contact = () => {
                     <Link href='/#top'>
                         <div className='dark:bg-[white] rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <HiOutlineChevronDoubleUp
-                                className='text-[#5651e5]'
+                                className='text-[rgb(16,129,212)]'
                                 size={30}
                             />
                         </div>
