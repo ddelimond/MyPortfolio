126530011c358c542820b2a280332a1b8bf58f3d
Darren Delimond <darrendelimond@gmail.com>
2023-12-25 05:36:50 -0500

    =updated card image border

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -6,7 +6,7 @@ const ProjectItem = ({ image, title, tools, url }) => {
     return (
         <div className="dark:bg-white md:w-[50%] ">
             <div className="flex relative justify-center items-center w-full  h-auto shadow-md shadow-gray-400  p-4 group hover:bg-gradient-to-r from-[rgb(16,129,212)] to-[rgb(14,178,243)] ">
-                <Image alt="project image" className="rounded-xl group-hover:opacity-30 w-full transition ease-in duration-200" src={image} />
+                <Image alt="project image" className=" group-hover:opacity-30 w-full transition ease-in duration-200" src={image} />
                 <div className="hidden group-hover:flex flex-col justify-center items-center absolute w-full h-full gap-y-6 transition ease-in duration-200">
                     <h3 className="text-white text-2xl tracking-wider">{title}</h3>
                     <span className="text-gray-200 ">{tools}</span>
