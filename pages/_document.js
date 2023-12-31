b825be74288aa71ae7ec09154e698a786764fcf3
Darren Delimond <darrendelimond@gmail.com>
2023-12-31 11:46:08 -0500

    =added dark mode to body tag

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,10 @@ export default function Document() {
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
             </Head>
-            <body>
+            <body className='bg-[#ecf0f3] dark:bg-[rgb(16,22,37)]'>
                 <Main />
                 <NextScript />
             </body>
         </Html>
     )
 }
-    
