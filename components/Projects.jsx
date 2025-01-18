20ec94329a26f302bd2cd76307f22b53f4854304
D2 <darrendelimond@gmail.com>
2025-01-18 16:09:47 -0500

    =added old projects back but remove links since im creating new ones

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -21,7 +21,11 @@ const Projects = () => {
                 <h2 className="font-bold py-4  dark:text-[rgb(14,178,243)]">What I&apos;ve Built</h2>
                 <section className="flex flex-col items-center md:w-[1240px] md:m-auto gap-[40px] md:flex-row md:flex-wrap  justify-center w-full md:max-w-[1240px]">
 
+                    <ProjectItem image={songly} title='Songly' tools='React, Tailwind, JavaScript' url='/songly' />
+                    <ProjectItem image={reelflix} title='Reelflix' tools='Html, CSS, JavaScript' url='/reelflix' />
                     <ProjectItem image={stash} title='Stash' tools='React, Express, Node' url='/stash' />
+                    <ProjectItem image={infintech} title='Infinite Tech' tools='React, Tailwind, JavaScript' url='/infintech' />
+
                 </section>
             </section>
         </div>
